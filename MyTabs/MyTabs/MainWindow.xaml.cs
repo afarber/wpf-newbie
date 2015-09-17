2bb7cc27ff7cd3a7b06e71bd4e3d15e7c383273c
Alexander Farber <Alexander.Farber@gmail.com>
2015-09-17 11:05:28 +0200

    Add MyTabs project with 5 tabs and a list box

---
+++ MyTabs/MyTabs/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyTabs
+{
+
+    public partial class MainWindow : Window
+    {
+        protected TabItem tabItemPreviousScreen = null;
+        protected TabItem tabItemCurrentScreen = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected delegate void delegate_SetScreen(TabItem tabItemOfScreenToSet);
+        protected void SetScreen(TabItem tabItemOfScreenToSet)
+        {
+            tabItemOfScreenToSet.IsSelected = true;
+            tabItemOfScreenToSet.Focus();
+            this.tabItemPreviousScreen = this.tabItemCurrentScreen;
+            this.tabItemCurrentScreen = tabItemOfScreenToSet;
+        }
+    }
+}
